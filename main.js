e14ff99acd8e48d5a06bb52fbe02ab9668f1ccb1
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2023-04-27 14:54:17 -0500

    Add reset button with choosable dimensions

--- main.js
+++ main.js
@@ -2,20 +2,31 @@ function onHover(event) {
     event.target.style.backgroundColor = 'black';
 }
 
-
-let container = document.querySelector('#container');
-let rows = [];
-for (let i = 0; i < 16; i++) {
-    let row = [];
-    const flexRow = document.createElement('div');
-    flexRow.setAttribute('class', 'row');
-    for (let j = 0; j < 16; j++) {
-        const gridSquare = document.createElement('div');
-        gridSquare.setAttribute('class', 'grid');
-        gridSquare.addEventListener('mouseover', (e) => onHover(e));
-        row.push(gridSquare);
-        flexRow.appendChild(gridSquare);
-    }
-    rows.push(row);
-    container.appendChild(flexRow);
+function generateGrid(dimensions) {
+    let container = document.querySelector('#container');
+    let rows = [];
+    for (let i = 0; i < dimensions; i++) {
+        let row = [];
+        const flexRow = document.createElement('div');
+        flexRow.setAttribute('class', 'row');
+        for (let j = 0; j < dimensions; j++) {
+            const gridSquare = document.createElement('div');
+            gridSquare.setAttribute('class', 'grid');
+            gridSquare.addEventListener('mouseover', (e) => onHover(e));
+            row.push(gridSquare);
+            flexRow.appendChild(gridSquare);
+        }
+        rows.push(row);
+        container.appendChild(flexRow);
+    }    
 }
+
+function resetGrid() {
+    let dimensions = Number(prompt('How many squares should there be for side?'));
+
+    generateGrid(dimensions);
+}
+
+let resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', () => resetGrid());
+resetGrid();
